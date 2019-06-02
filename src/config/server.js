d934fe7c3582eddb8f6ee539a705d7ac3b6f559a
Felipe Alves <felipealves.romao@gmail.com>
2019-06-02 00:42:07 -0300

    Adding cors in the application

--- src/config/server.js
+++ src/config/server.js
@@ -1,10 +1,11 @@
 const bodyParser = require('body-parser')
 const express = require('express')
-
+const allowCors = require('./cors')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(3333)
 
